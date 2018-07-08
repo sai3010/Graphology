496f07ccad153c7ab0c93e11b16e02da66f3abb3
spoorthishivaswamy <s.spoorthi.97@gmail.com>
2018-07-08 15:04:14 +0530

    navbar scroll done

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ setTimeout(function() {
   });
         
   /*nav bar scroll*/
- /* window.onscroll = function() {scrollFunction()};
+  /*window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -29,6 +29,23 @@ function scrollFunction() {
         document.getElementById("navbar").style.top = "-100px";
     }
 }*/
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 
 $(document).ready(function(){
     $('.collapsible').collapsible();
